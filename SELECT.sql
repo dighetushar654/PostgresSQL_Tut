5696a6ddc7811652260b653b77974672bdaca8e2
Tushar Dighe <64326412+dighetushar654@users.noreply.github.com>
2021-05-04 20:55:48 +0530

    Update SELECT.sql

--- SELECT.sql
+++ SELECT.sql
@@ -91,3 +91,104 @@ OUTPUT:
        15
 (1 row)
 *************************************************************************************************************************************************************************
+6) Using PostgreSQL ORDER BY clause to sort rows by one column
+The following query uses the ORDER BY clause to sort customers by their first names in ascending order:
+
+SELECT first_name, last_name FROM customer ORDER BY first_name ASC;
+
+OUTPUT:
+first_name  |  last_name
+-------------+--------------
+ Aaron       | Selby
+ Adam        | Gooch
+ Adrian      | Clary
+ Agnes       | Bishop
+ Alan        | Kahn
+ Albert      | Crouse
+ Alberto     | Henning
+ Alex        | Gresham
+ Alexander   | Fennell
+ Alfred      | Casillas
+ Alfredo     | Mcadams
+ Alice       | Stewart
+ Alicia      | Mills
+ Allan       | Cornish
+ Allen       | Butterfield
+ Allison     | Stanley
+ Alma        | Austin
+ Alvin       | Deloach
+ Amanda      | Carter
+ Amber       | Dixon
+ Amy         | Lopez
+ Ana         | Bradley
+ Andre       | Rapp
+ **********************************************************************************************************************************************************************
+ 7) Using PostgreSQL ORDER BY clause to sort rows by one column in descending order
+The following statement selects the first name and last name from the customer table and sorts the rows by values in the last name column in descending order:
+
+SELECT first_name, last_name FROM customer ORDER BY last_name DESC;
+OutPut:
+first_name  |  last_name
+-------------+--------------
+ Cynthia     | Young
+ Marvin      | Yee
+ Luis        | Yanez
+ Brian       | Wyman
+ Brenda      | Wright
+ Tyler       | Wren
+ Florence    | Woods
+ Lori        | Wood
+ Virgil      | Wofford
+ Darren      | Windham
+ Susan       | Wilson
+ Bernice     | Willis
+ Gina        | Williamson
+ Linda       | Williams
+ Jon         | Wiles
+ Roy         | Whiting
+ Betty       | White
+ Lucy        | Wheeler
+ Fred        | Wheat
+ Mitchell    | Westmoreland
+ Edna        | West
+ Sheila      | Wells
+ Marlene     | Welch
+ Ronald      | Weiner
+ ***************************************************************************************************************************************************************************
+ 8) First, use the following CREATE TABLE statement to create the distinct_demo table that consists of three columns: id, bcolorand fcolor.
+
+CREATE TABLE distinct_demo (id serial NOT NULL PRIMARY KEY, bcolor VARCHAR, fcolor VARCHAR);
+
+INSERT INTO distinct_demo (bcolor, fcolor) VALUES	('red', 'red'),	('red', 'red'),	('red', NULL),	(NULL, 'red'),	('red', 'green'),	('red', 'blue'),	('green', 'red'),	('green', 'blue'),	('green', 'green'), ('blue', 'red'), ('blue', 'green'), ('blue', 'blue');
+ 
+Third, query the data from the distinct_demo table using the SELECT statement:
+
+SELECT id,	bcolor,	fcolor FROM	distinct_demo ;
+ 
+ OutPut:
+  id | bcolor | fcolor
+----+--------+--------
+  1 | red    | red
+  2 | red    | red
+  3 | red    |
+  4 |        | red
+  5 | red    | green
+  6 | red    | blue
+  7 | green  | red
+  8 | green  | blue
+  9 | green  | green
+ 10 | blue   | red
+ 11 | blue   | green
+ 12 | blue   | blue
+(12 rows)
+***************************************************************************************************************************************************************************
+SELECT	DISTINCT bcolor FROM	distinct_demo ORDER BY	bcolor;
+ 
+ bcolor
+--------
+ blue
+ green
+ red
+
+(4 rows)
+******************************************************************************************************************************************************
